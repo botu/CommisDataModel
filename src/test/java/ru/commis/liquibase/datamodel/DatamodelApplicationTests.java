bad7e3d32dcef417842abb06670c633ba2bc5686
borisovtimofey <borisovtimofey@gmail.com>
2022-12-24 18:24:38 +0300

    first

---
+++ src/test/java/ru/commis/liquibase/datamodel/DatamodelApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.commis.liquibase.datamodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatamodelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
